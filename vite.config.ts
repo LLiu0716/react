c89223d087020bb13e8f4322c6fcce312eb1ddb5
liuliu <450441058@qq.com>
2021-01-12 19:26:27 +0800

    init

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,24 @@
 import * as reactPlugin from 'vite-plugin-react'
 import type { UserConfig } from 'vite'
+const path = require( 'path' )
 
 const config: UserConfig = {
   jsx: 'react',
-  plugins: [reactPlugin]
+  plugins: [ reactPlugin ],
+  port: 8888,
+  open: true,
+  // https: false,
+  // ssr: false,
+  // optimizeDeps: {
+  //   include: [ '' ]
+  // },
+  // alias: {
+  //   '/@/': path.resolve( __dirname, './src' )
+  // }
+  // proxy: {
+  //   '/foo': ''
+  // }
 }
 
 export default config
+
